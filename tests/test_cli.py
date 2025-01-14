16f6af0234faa349fe6068c6209e4f1b823739e6
zackees <z@zackees.com>
2025-01-13 18:19:22 -0800

    chore: update project structure and configuration files

---
+++ tests/test_cli.py
@@ -0,0 +1,17 @@
+"""
+Unit test file.
+"""
+
+import unittest
+
+
+class MainTester(unittest.TestCase):
+    """Main tester class."""
+
+    def test_imports(self) -> None:
+        """Test command line interface (CLI)."""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()

643010c76abbd1ad76c250c981812664678156bf
zackees <z@zackees.com>
2025-01-15 01:05:08 -0800

    refactor: rename dist_out_idr to dist_dir in build_wheel function

--- src/digital_ocean_cluster/build_wheel.py
+++ src/digital_ocean_cluster/build_wheel.py
@@ -10,13 +10,13 @@ assert (PROJECT_ROOT / "pyproject.toml").exists()
 PYTHON_EXE = sys.executable
 
 
-def build_wheel(setup_py: Path, dist_out_idr: Path, python: str | None = None) -> None:
+def build_wheel(setup_py: Path, dist_dir: Path, python: str | None = None) -> None:
     python = python or PYTHON_EXE
     if not setup_py.exists():
         raise FileNotFoundError(f"setup.py not found: {setup_py}")
     if setup_py.name != "setup.py":
         raise ValueError(f"Input setup_py must be setup.py: {setup_py}")
-    dist_out_idr.parent.mkdir(exist_ok=True, parents=True)
+    dist_dir.parent.mkdir(exist_ok=True, parents=True)
     project_root = setup_py.parent
     # python setup.py bdist_wheel
     subprocess.run(

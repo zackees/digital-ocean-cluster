4996790a2a4cc5a27bf9ebe0ce67864f376ca61f
zackees <z@zackees.com>
2025-01-15 17:57:26 -0800

    refactor: move MACHINE_SIZES to a separate module

--- src/digital_ocean_cluster/api.py
+++ src/digital_ocean_cluster/api.py
@@ -9,6 +9,7 @@ from tempfile import TemporaryDirectory
 from typing import Any
 
 from digital_ocean_cluster.ensure_doctl import ensure_doctl
+from digital_ocean_cluster.machines import MACHINE_SIZES
 
 _TIME_DELETE_BEFORE_GONE = 10
 SLEEP_TIME_BEFORE_SSH = 10
@@ -37,69 +38,6 @@ class SSHKey:
 
     def __str__(self) -> str:
         return f"SSHKey: name={self.name},id={self.id},fingerprint={self.fingerprint}"
-
-
-MACHINE_SIZES = [
-    "s-1vcpu-512mb-10gb",
-    "s-1vcpu-1gb",
-    "s-1vcpu-1gb-amd",
-    "s-1vcpu-1gb-intel",
-    "s-1vcpu-1gb-35gb-intel",
-    "s-1vcpu-2gb",
-    "s-1vcpu-2gb-amd",
-    "s-1vcpu-2gb-intel",
-    "s-1vcpu-2gb-70gb-intel",
-    "s-2vcpu-2gb",
-    "s-2vcpu-2gb-amd",
-    "s-2vcpu-2gb-intel",
-    "s-2vcpu-2gb-90gb-intel",
-    "s-2vcpu-4gb",
-    "s-2vcpu-4gb-amd",
-    "s-2vcpu-4gb-intel",
-    "s-2vcpu-4gb-120gb-intel",
-    "s-2vcpu-8gb-amd",
-    "c-2",
-    "c2-2vcpu-4gb",
-    "s-2vcpu-8gb-160gb-intel",
-    "s-4vcpu-8gb",
-    "s-4vcpu-8gb-amd",
-    "s-4vcpu-8gb-intel",
-    "g-2vcpu-8gb",
-    "s-4vcpu-8gb-240gb-intel",
-    "gd-2vcpu-8gb",
-    "g-2vcpu-8gb-intel",
-    "gd-2vcpu-8gb-intel",
-    "s-4vcpu-16gb-amd",
-    "m-2vcpu-16gb",
-    "c-4",
-    "c2-4vcpu-8gb",
-    "s-4vcpu-16gb-320gb-intel",
-    "s-8vcpu-16gb",
-    "m-2vcpu-16gb-intel",
-    "m3-2vcpu-16gb",
-    "c-4-intel",
-    "m3-2vcpu-16gb-intel",
-    "s-8vcpu-16gb-amd",
-    "s-8vcpu-16gb-intel",
-    "c2-4vcpu-8gb-intel",
-    "g-4vcpu-16gb",
-    "s-8vcpu-16gb-480gb-intel",
-    "so-2vcpu-16gb-intel",
-    "so-2vcpu-16gb",
-    "m6-2vcpu-16gb",
-    "gd-4vcpu-16gb",
-    "so1_5-2vcpu-16gb-intel",
-    "g-4vcpu-16gb-intel",
-    "gd-4vcpu-16gb-intel",
-    "so1_5-2vcpu-16gb",
-    "s-8vcpu-32gb-amd",
-    "m-4vcpu-32gb",
-    "c-8",
-    "c2-8vcpu-16gb",
-    "s-8vcpu-32gb-640gb-intel",
-    "m-4vcpu-32gb-intel",
-    "m3-4vcpu-32gb",
-]
 
 
 WINDOWS_OPENSSH = "C:\\Windows\\System32\\OpenSSH\\ssh.exe"
